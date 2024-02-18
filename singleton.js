48fb2b3eb110c11ecce1c1540efc93561c3612bb
Alen Wuhl <128154613+Alenwuhl@users.noreply.github.com>
2024-02-18 15:17:25 -0300

    Add files via upload

---
+++ singleton.js
@@ -0,0 +1,23 @@
+let instance = null
+
+class SingletonClass {
+    constructor() {
+        this.value = Math.random(100)
+    }
+
+
+    static getInstance() {
+        // Sin singleton
+        // instance = new SingletonClass();
+        // return instance
+
+        // simulacion Con singleton
+        if (!instance) {
+            instance = new SingletonClass();
+        }
+
+        return instance
+    }
+}
+
+export default SingletonClass;
